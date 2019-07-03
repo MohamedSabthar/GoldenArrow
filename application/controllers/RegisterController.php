3f36cd03697d652d76afc12356b479a22b056abe
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2019-07-03 15:28:13 +0530

    Fixed login page incorect controller function call

--- application/controllers/RegisterController.php
+++ application/controllers/RegisterController.php
@@ -1,6 +1,6 @@
 
 <?php
-    class registerController extends CI_Controller{
+    class RegisterController extends CI_Controller{
         public function __construct()
         {
             parent ::__construct();
