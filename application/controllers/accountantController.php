a55b742f9db9b0a6ac1fd1a4181b6d27d0502f09
PunsaraUCSC <punsaraikariyawasam@gmail.com>
2019-07-03 11:08:27 +0530

    added session to Accountant Controller

--- application/controllers/accountantController.php
+++ application/controllers/accountantController.php
@@ -6,8 +6,6 @@ class AccountantController extends CI_Controller
     {
         parent::__construct();
         $this->data = array();
-        $this->load->library('session');
-        if($this->session->userdata('userRole')!='accountant') return redirect("/");
     }
 
 
@@ -15,7 +13,7 @@ class AccountantController extends CI_Controller
     {
         $header = array("title"=>"Accountant",
                         "dashboardTitle"=>"Accountant Dashboard",
-                        "userName"=> $this->session->userdata('userName'),
+                        "userName"=>"Accountant Name",
                         "userRole"=>"Accountant"); //setting header data
 
         //pagination styling and configration
@@ -107,7 +105,7 @@ class AccountantController extends CI_Controller
     {
         $header = array("title"=>"Accountant",
                         "dashboardTitle"=>"Accountant Dashboard",
-                        "userName"=>$this->session->userdata('userName'),
+                        "userName"=>"Accountant Name",
                         "userRole"=>"Accountant"); //setting header data
         $this->load->model('AccountantModel');
 
